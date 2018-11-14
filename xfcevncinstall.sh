b019510aa27c9d92eae43009606278237bbd5716
zhaorui <25546385+zhaorui963@users.noreply.github.com>
2018-11-14 09:35:53 +0800

    Update xfcevncinstall.sh

--- xfcevncinstall.sh
+++ xfcevncinstall.sh
@@ -12,10 +12,13 @@ systemctl restart xrdp &&
 systemctl start xrdp &&
 systemctl enable xrdp &&
 systemctl daemon-reload &&
+echo '请输入密码' &&
 vncpasswd &&
 systemctl daemon-reload &&
 systemctl enable vncserver@:1.service &&
 systemctl start vncserver@:1.service &&
+systemctl stop firewalld &&
+systemctl disable firewalld &&
 echo 'All is ok!'
 
 
