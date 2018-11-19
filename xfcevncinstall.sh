f884bfe6a3e3770e964a1ce127ce6e2b456207b3
zhaorui <25546385+zhaorui963@users.noreply.github.com>
2018-11-19 18:48:06 +0800

    Update xfcevncinstall.sh

--- xfcevncinstall.sh
+++ xfcevncinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yum update -y && yum install epel-release -y && yum groups install Xfce -y && 
+yum install epel-release -y && yum update -y && yum groups install Xfce -y && 
 yum install -y wqy* ibus.x86_64 ibus-libpinyin.x86_64 im-chooser.x86_64 xrdp tigervnc tigervnc-server net-tool &&
 cd /root/xfcevnc &&
 cp -f locale.conf /etc/locale.conf &&
